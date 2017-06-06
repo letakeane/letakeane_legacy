227dd51b2d7954e845a7e635e503c50122becbbe
Leta Keane <letakeane+turing@gmail.com>
2017-06-06 15:11:15 -0600

    Initial commit: setup file structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Router, Switch, Route, Link } from 'react-router-dom';
+import { Aside } from './Aside';
+import './assets/stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
