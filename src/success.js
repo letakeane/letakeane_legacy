415265c894c59fb3d30391d84f3f8439dfd93709
Leta Keane <letakeane+turing@gmail.com>
2017-06-07 14:24:26 -0600

    Style resume; change apostrophes to HTML symbol

--- src/success.js
+++ src/success.js
@@ -5,7 +5,7 @@ export const success = () => {
   return (
     <article className='success'>
       <h2 className='form-greeting'>Thanks for your note!</h2>
-      <h2 className='form-greeting'>I'll get back to you soon.</h2>
+      <h2 className='form-greeting'>I&#8217;ll get back to you soon.</h2>
     </article>
   )
 }
